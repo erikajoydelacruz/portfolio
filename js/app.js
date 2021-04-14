b982ca30ee3f762096bebdccfead52ebe9306857
Erika Joy De La Cruz <erikajoy.odc@gmail.com>
2021-04-13 17:34:40 -0700

    update js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
+console.log("hi");
+
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+const work = document.getElementById("work");
 
-function displayModal()
+
+const menuBtn = document.querySelector(".hamburger");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+    console.log("hey");
+    } else {
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+    }
+});

2be5009ac88c7f2ec9cb85c2b3db559c7fe5dc48
Erika Joy De La Cruz <erikajoy.odc@gmail.com>
2021-04-14 17:43:36 -0700

    create hamburger functionality

--- js/app.js
+++ js/app.js
@@ -5,15 +5,28 @@ const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
 const work = document.getElementById("work");
 
-
+/*
 const menuBtn = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-link");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        menuOpen = true;
+        nav.style.display="block";
     console.log("hey");
+    
     } else {
         menuBtn.classList.remove("open");
         menuOpen = false;
+        console.log("yo");
+        nav.style.display="";
     }
+}); */
+
+const hamburger = document.querySelector(".hamburger");
+const navUL = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+    navUL.classList.toggle("show");
 });

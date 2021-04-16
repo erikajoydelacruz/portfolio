358c723da19a13a9760ee31976e79dda919df74a
Erika Joy De La Cruz <erikajoy.odc@gmail.com>
2021-04-15 17:37:13 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,36 @@
-console.log("hi");
-
-const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal-content");
-const modalClose = document.querySelector(".modal-close");
-const work = document.getElementById("work");
-
-/*
-const menuBtn = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav-link");
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-        nav.style.display="block";
-    console.log("hey");
-    
-    } else {
-        menuBtn.classList.remove("open");
-        menuOpen = false;
-        console.log("yo");
-        nav.style.display="";
-    }
-}); */
-
+//MOBILE MENU
 const hamburger = document.querySelector(".hamburger");
 const navUL = document.getElementById("nav");
 
 hamburger.addEventListener("click", () => {
     navUL.classList.toggle("show");
 });
+
+//MODAL
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+const work = document.querySelectorAll(".img-thumbnail");
+
+/*
+function displayModal() {
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;  
+} */
+
+const form = document.getElementById("form");
+const overlayForm = document.getElementById("form-overlay");
+
+form.addEventListener("click", () => {
+    overlayForm.classList.remove("hidden")
+    console.log("hey");
+})
+
+/*
+for (let i=0; i<work.length; i++) {
+    work[i].addEventListener("click", function () {
+        console.log("yo");
+        displayModal();
+    });
+}; */
+
